544858ec6873c7fad71b2e2a3d11a22ebed28d87
wuxiaojun <wuxiaojun@wuxiaojundeMacBook-Pro.local>
2023-12-15 14:22:01 +0800

    update: readme

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# 用python和gradio实现摄像头的调用
-def startCamera():
-    # start the camera and pump the video into the #cameraCanvas canvas
 
-    # const video = document.querySelector("video") as HTMLVideoElement;
-
-    # navigator.mediaDevices
-    #   .getUserMedia({
-    #     audio: true,
-    #     video: true,
-    #   })
-    #   .then((stream) => {
-    #     video.srcObject = stream;
-    #     video.play();
-
-    #     copyToCanvas();
-    #   })
-    #   .catch(function (e) {
-    #     if (
-    #       confirm(
-    #         "An error with camera occurred:(" + e.name + ") Do you want to reload?"
-    #       )
-    #     ) {
-    #       location.reload();
-    #     }
-    #   });
+if __name__ == "__main__":
     pass
-    
