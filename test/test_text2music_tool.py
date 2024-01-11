eec7fe212e724f396b626701a4b8ae2e7aed9838
Kunhao, Pan <pankunhao@gmail.com>
2024-01-11 11:31:56 +0800

    direct return audio (#34)
    * direct return audio

--- test/test_text2music_tool.py
+++ test/test_text2music_tool.py
@@ -2,17 +2,17 @@
 
 import os
 import sys
+import json
 import argparse
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from real_gemini.agent import ReActAgent
+from real_gemini.tools.music_tool import Text2MusicTool
 
 if __name__ == '__main__':
     # arguments containing: config_path, ckpt_path, max_length
-    args_parser = argparse.ArgumentParser("test gpt4v for langchain tools")
+    args_parser = argparse.ArgumentParser("test music tool")
     args_parser.add_argument("--prompt", type=str, help="prompt string", required=True)
-    args_parser.add_argument("--image_path_or_dir", type=str, help="image path or dir", required=True)
     args = args_parser.parse_args()
 
     work_dir = os.path.dirname(os.path.dirname(__file__))
@@ -20,8 +20,6 @@ if __name__ == '__main__':
     from dotenv import load_dotenv
     load_dotenv(dotenv_path=env_file)
 
-    # image dir是过去一段时间截帧的图片目录
-    agent = ReActAgent()
-    # agent = SimpleRouterAgent(image_dir=args.image_dir)
-    output = agent.run(prompt=args.prompt, image_path_or_dir=args.image_path_or_dir)
-    print("output:", output)
+    tool = Text2MusicTool()
+    resp = tool.inference(args.prompt)
+    print("resp:", resp)
