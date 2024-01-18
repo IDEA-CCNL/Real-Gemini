40c851ebbf33a0998afeaeb45da8a5ef4ddb9701
Kunhao, Pan <pankunhao@gmail.com>
2024-01-18 15:35:12 +0800

    minor fix (#40)

--- real_gemini/tools/image_generation_tool.py
+++ real_gemini/tools/image_generation_tool.py
@@ -4,21 +4,43 @@ import os
 import json
 import hashlib
 import requests
+from langchain.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 from ..utils.image_stacker import save_or_show_image
 
 class TaiyiGeneralTool(object):
     _name_ = "taiyi general image generation"
-    _description_ = "Taiyi General的API，用于从文本生成图像。当你需要从文本描述生成图像时非常有用。输入应该是文本，即图像描述。\nA wrapper around Taiyi General API for text to image generation. Useful for when you need to generate images from a text description. Input should be text, i.e, an image description."
+    _description_ = "Taiyi General的API，用于从文本生成图像。当你需要从文本描述生成图像时非常有用。输入应该是文本，即图像描述。A wrapper around Taiyi General API for text to image generation. Useful for when you need to generate images from a text description. Input should be text, i.e, an image description."
     _return_direct_ = True
 
     def __init__(self):
+        self.prompter = ChatOpenAI(
+            model="gpt-3.5-turbo",
+            max_tokens=256)
         self.host = os.getenv("IMAGE_GENERATION_SERVER_HOST")
         self.port = os.getenv("IMAGE_GENERATION_SERVER_PORT")
     
+    def _upgrade_prompt(self, prompt):
+        messages = []
+        messages.append(
+            SystemMessage(
+                content=[
+                    {"type": "text", "text": "我正在使用一个Stable Diffusion的AI图像生成工具，我想让你充当我的prompt优化生成器。在我想生成的主题后，请帮我添加各种关键词，使得我的主题的描述更加详细，添加的关键词包括：主体、背景效果、风格、拍摄方式。例如，如果我输入“跑车”，你将生成关键词，如：“跑车,高清,4k,真实细致的跑车摄影,速度动态模糊,赛车场,城市环境,风景道路,戏剧性的天空”"}
+                ]
+            )
+        )
+        messages.append(HumanMessage(content=prompt))
+
+        response_msg = self.prompter.invoke(messages)
+        new_prompt = response_msg.content
+        return new_prompt
+
     def inference(self, inputs):
         url = f"http://{self.host}:{self.port}/taiyi_xl_general_base64/"
         headers = {"Content-Type": "application/json"}
-        data = {"prompt": inputs}
+        new_prompt = self._upgrade_prompt(inputs)
+        print("new prompt:", new_prompt)
+        data = {"prompt": new_prompt}
         response = requests.post(url, headers=headers, data=json.dumps(data))
         response = response.json()
         b64_image = response["image_base64"]
--- real_gemini/tools/sam_tool.py
+++ real_gemini/tools/sam_tool.py
@@ -2,12 +2,7 @@ import requests
 
 class SegmentingTool(object):
     _name_="Segment the Image"
-    _description_="""当你想要对图片中的所有物体进行分割，但是不想对某个物体进行分割时很有用。比如：分割这张图片中的所有物体，或者在这张图片上生成分割结果，或者对这张图片进行分割，或者分割这张图片中的所有物体。输入到这个工具的应该是一个字符串，表示图片的路径或者图片所在的文件夹路径。
-                useful when you want to segment all the part of the image, but not segment a certain object.like: segment all the object in this image, or generate segmentations on this image,
-                or segment the image,"
-                or perform segmentation on this image, "
-                or segment all the object in this image."
-                The input to this tool should be a string, representing the image_dir or image_paths."""
+    _description_="当你想要对图片中的所有物体进行分割，但是不想对某个物体进行分割时很有用。比如：分割这张图片中的所有物体，或者在这张图片上生成分割结果，或者对这张图片进行分割，或者分割这张图片中的所有物体。输入到这个工具的应该是一个字符串，表示图片的路径或者图片所在的文件夹路径。useful when you want to segment all the part of the image, but not segment a certain object.like: segment all the object in this image, or generate segmentations on this image, or segment the image, or perform segmentation on this image, or segment all the object in this image. The input to this tool should be a string, representing the image_dir or image_paths."
     _return_direct_ = False
     
     def __init__(self):
