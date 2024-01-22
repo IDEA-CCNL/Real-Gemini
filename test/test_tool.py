4ed73be92f343dde44307102d02943884d91ac6d
liu yibo <liuyibo@idea.edu.cn>
2024-01-22 17:47:17 +0800

    debug qwen_vl api

--- test/test_tool.py
+++ test/test_tool.py
@@ -10,14 +10,14 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from real_gemini.tools.music_tool import Text2MusicTool
 from real_gemini.tools.image_generation_tool import TaiyiGeneralTool
 from real_gemini.tools.weather_tool import WeatherTool
-# from real_gemini.tools.tts_tool import TTSTool
+from real_gemini.tools.tts_tool import TTSTool
 from real_gemini.tools.qwen4v_tool import QWEN4VTool
 
 TOOL_DICT = {
     "music": Text2MusicTool,
     "image": TaiyiGeneralTool,
     "weather": WeatherTool,
-    # "tts": TTSTool,
+    "tts": TTSTool,
     "qwen4v": QWEN4VTool
 }
 
