204d9686da3f34bb16fe65bf7f0b7a64e429e0cb
rookielyb <50746135+rookielyb@users.noreply.github.com>
2024-01-22 18:49:09 +0800

    Dev liuyibo (#42)
    * add test images
    
    * delete images
    
    * add test images
    
    * change test images
    
    * chage images
    
    * chage images
    
    * add image selector
    
    * fix image_select bug
    
    * adjust image order
    
    * add qwen_vl api
    
    * debug qwen_vl api
    
    * debug qwen_vl api
    
    ---------
    
    Co-authored-by: liu yibo <liuyibo@idea.edu.cn>
    Co-authored-by: Kunhao, Pan <pankunhao@gmail.com>

--- test/test_agent.py
+++ test/test_agent.py
@@ -23,5 +23,8 @@ if __name__ == '__main__':
     # image dir是过去一段时间截帧的图片目录
     agent = ReActAgent()
     # agent = SimpleRouterAgent(image_dir=args.image_dir)
+    # args.prompt = "这是正确的顺序吗？考虑与太阳的距离并解释你的推理。"
+    # args.image_path_or_dir = os.path.join(work_dir, "test/images/reasoning_logic/reasoning_logic_1.png")
+
     output = agent.run(prompt=args.prompt, image_path_or_dir=args.image_path_or_dir)
     print("output:", output)
